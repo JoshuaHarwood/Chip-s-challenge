0664e47d7c2a645873af00886f4773302dbe8eb2
JoshuaHarwood <38898198+JoshuaHarwood@users.noreply.github.com>
2019-09-23 00:05:10 +1200

    the view now follows the player when the board is shrunk
    work is still needed as when the player is far to each wall black bars are on the side of the map.

--- src/nz/ac/vuw/ecs/swen225/a3/render/BoardCanvas.java
+++ src/nz/ac/vuw/ecs/swen225/a3/render/BoardCanvas.java
@@ -1,8 +1,10 @@
 package nz.ac.vuw.ecs.swen225.a3.render;
 
 import com.sun.javafx.fxml.builder.JavaFXImageBuilder;
+import nz.ac.vuw.ecs.swen225.a3.maze.Chap;
 import nz.ac.vuw.ecs.swen225.a3.maze.Maze;
 import nz.ac.vuw.ecs.swen225.a3.maze.Tile;
+import nz.ac.vuw.ecs.swen225.a3.maze.TileType;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -14,7 +16,7 @@ import java.awt.image.BufferedImage;
 public class BoardCanvas extends Canvas {
 
     private int tileSize = 32;
-    private int minTileSize = 32;
+    private int minTileSize = 64;
     private Tile[][] tiles;
     private int cols, rows;
     
@@ -58,15 +60,15 @@ public class BoardCanvas extends Canvas {
         if(tileSize < minTileSize){
             tileSize = minTileSize;
 
-            int viewW = this.getWidth() / minTileSize;
-            int viewH = this.getHeight() / minTileSize;
-            view = new Tile[viewH][viewW]; //the new view of the player, this will leave out some
+            int viewW = w / minTileSize;
+            int viewH = h / minTileSize;
 
-            int chapX = maze.getChap().getX();
-            int chapY = maze.getChap().getY();
+            int tilesToLeft = maze.getChap().getX() -1;
+            int tilesToRight = tiles[0].length - (maze.getChap().getX() + 1);
+            int tilesToTop = maze.getChap().getY() -1;
+            int tilesToBottom = tiles.length - maze.getChap().getY();
 
-            
-
+            view = adjustView(viewH, viewW, tilesToLeft, tilesToRight, tilesToTop, tilesToBottom);
 
         } else {
             view = tiles;
@@ -85,16 +87,75 @@ public class BoardCanvas extends Canvas {
         //go through each of the tiles and draw them
         for (int i = 0; i < view[0].length; i++) {
             for (int j = 0; j < view.length; j++) {
-//
-                Image tileImg = view[j][i].getImage(); // get the image
-                int x = i * tileSize; //work the X and Y
-                int y = j * tileSize;
 
-                imgG.drawImage(tileImg, x, y, tileSize, tileSize, this); //draw the image
+                if((view[j][i] != null)) {
+                    Image tileImg = view[j][i].getImage(); // get the image
+                    int x = i * tileSize; //work the X and Y
+                    int y = j * tileSize;
+
+                    imgG.drawImage(tileImg, x, y, tileSize, tileSize, this); //draw the image
+                }
             }
         }
 
         this.getGraphics().drawImage(image, 0, 0, this);
         imgG.dispose();
+    }
+
+    /**
+     * generate a view given the parameters
+     * @param viewH - the height of the view (tiles high)
+     * @param viewW - the width of the view (tiles wide)
+     * @param tilesToLeft - tiles to the left of the player (on the board)
+     * @param tilesToRight - tiles to the right of the player (on the board)
+     * @param tilesToTop - tiles above the player (on the board)
+     * @param tilesToBottom - tiles below the player (on the board)
+     * @return - returns a view within toes parameters
+     */
+    private Tile[][] adjustView(int viewH, int viewW, int tilesToLeft, int tilesToRight, int tilesToTop, int tilesToBottom) {
+
+        Tile[][] view = new Tile[viewH][viewW]; //the new view of the player, this will leave out some
+
+        System.out.println("ADJUSTING VIEW:   H:" + viewH + "   W:" + viewW);
+            //int windowSize = Math.min(viewH, viewW);
+            //System.out.println("    WINDOW SIZE" + windowSize);
+            //getting the tiles above and on the chap
+        int vX = 0, vY = 0; // position in the view
+
+        int y = maze.getChap().getY() - viewH/2; //we want to start getting
+        int x = maze.getChap().getX() - viewW/2;
+        int extraY = 0;
+        int extraX = 0;
+
+        if (viewH/2 > tilesToTop) {
+            extraY = viewH/2 - tilesToTop;
+            System.out.println("EXTRA Y: " + extraY);
+            y = 0;
+        }
+        if (viewW/2 > tilesToLeft) {
+            extraX = viewW/2 - tilesToLeft;
+            System.out.println("EXTRA X: " + extraX);
+            x = 0;
+        }
+
+
+
+        outer : while(true){
+            if(y >= 0 && y < tiles.length){ // if we have not gone out of the array
+                for (; x < maze.getChap().getX() + viewW/2; x++) { //same for X
+                    if(x >= 0 && x < tiles[0].length){ //we haven't gone out of the array
+
+                        view[vY][vX++] = tiles[y][x]; //update the view
+                    }
+                }
+                x = (viewW/2 > tilesToLeft) ? 0 : maze.getChap().getX() - viewW/2;
+                vY++;
+                vX = 0;
+            }
+            if(++y >= tiles.length || vY >= view.length){ //if we have reached chap
+                break; //break out
+            }
+        }
+      return view;
     }
 }
