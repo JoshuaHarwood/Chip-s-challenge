e512e28a1e1da396babca36cdc9a3451e1a931a1
hidetliam <hidetliam@myvuw.ac.nz>
2019-10-10 10:17:59 +1300

    Created method stubs for Json handling, as well as added libraries

--- src/nz/ac/vuw/ecs/swen225/a3/persistence/Persistence.java
+++ src/nz/ac/vuw/ecs/swen225/a3/persistence/Persistence.java
@@ -1,4 +1,32 @@
 package nz.ac.vuw.ecs.swen225.a3.persistence;
+import nz.ac.vuw.ecs.swen225.a3.maze.Maze;
+import javax.json.JsonObject;
 
 public class Persistence {
+
+
+
+
+    //Handles conversion of a maze object into a Json type file
+    static void save(Maze maze){
+
+
+
+
+
+    }
+
+    //Handles conversion of a Json type file into a maze object
+    static void load(JsonObject json){
+
+
+
+    }
+
+    //Handles write of a json object into a file
+    static void writeToFile(JsonObject json){
+
+    }
+
+
 }
