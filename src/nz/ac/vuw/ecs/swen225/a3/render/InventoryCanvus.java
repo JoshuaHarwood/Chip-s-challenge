4000e76c42db4df2e66bc40ddde8d84e5ff775b1
JoshuaHarwood <38898198+JoshuaHarwood@users.noreply.github.com>
2019-10-10 14:31:38 +1300

    Create InventoryCanvus.java

---
+++ src/nz/ac/vuw/ecs/swen225/a3/render/InventoryCanvus.java
@@ -0,0 +1,17 @@
+package nz.ac.vuw.ecs.swen225.a3.render;
+
+import java.awt.Canvas;
+
+/**
+ * 
+ * @author Joshua Harwood---300439084
+ *
+ */
+public class InventoryCanvus extends Canvas {
+	
+	
+	public void draw() {
+		
+	}
+
+}
