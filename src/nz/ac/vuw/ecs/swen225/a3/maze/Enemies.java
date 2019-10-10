7f50f976f7cffa1b73f299c5e34d6fd33a61f6b7
JoshuaHarwood <38898198+JoshuaHarwood@users.noreply.github.com>
2019-10-10 13:50:41 +1300

    Update Enemies.java
    added the move array in enemies

--- src/nz/ac/vuw/ecs/swen225/a3/maze/Enemies.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/Enemies.java
@@ -1,12 +1,30 @@
 package nz.ac.vuw.ecs.swen225.a3.maze;
 
+import java.util.ArrayList;
+
 /**
  * the enemies class (these are the mobs that will move around and attack the player)
  * @author Joshua Harwood.
  */
 public class Enemies extends GameCharacter {
 
-	public Enemies(int x, int y) {
+	ArrayList<Character> moves;
+	int currentMove = 0;
+	
+	public Enemies(ArrayList<Character> moves, int x, int y) {
 		super(TileType.Enemy, x, y);
+		this.moves = moves;
 	}
+	
+	public Character getNextMove() {
+		
+		if(moves.size() == 0) { return null; } //there are no moves
+		
+		if(currentMove >= moves.size()) {
+			currentMove = 0;
+		}
+		return moves.get(currentMove++);
+	}
+	
+	
 }
