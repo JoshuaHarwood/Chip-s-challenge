6f0852ed1141e815b4cb56333d7a7ead4601e3a7
joshuajhindley <38927613+joshuajhindley@users.noreply.github.com>
2019-10-11 23:55:58 +1300

    Last minute SpotBugs fix.

--- src/nz/ac/vuw/ecs/swen225/a3/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/a3/application/Main.java
@@ -141,14 +141,11 @@ public class Main {
 	 */
 	public Main(Maze m, int l, boolean loadingFromSave) {
 
-		if (loadingFromSave) {
-			maze = m;
-		}
-		else {
+		if (!loadingFromSave) {
 			updateMaze(l);
 		}
 
-		init(maze);
+		init(m);
 		initKeys();
 	}
 
